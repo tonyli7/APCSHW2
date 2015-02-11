5b71c41ee32f00b021d0f56725e221b83fd00983
tonyli7 <xxtonyxxlixx@gmail.com>
2015-02-11 00:49:12 -0500

    maze

--- 02maze/Driver.java
+++ 02maze/Driver.java
@@ -1,13 +1,15 @@
 public class Driver{
-    public static void main(String[]args){
-	
-	char[][]zz={{'X','X','X','X'},
-		    {'X','S',' ',' '},
-		    {'X','X','X',' '},
-		    {' ',' ',' ',' '}};
 
-	Maze m=new Maze(zz);
-	System.out.println(m.mazeSolve(zz));
-	System.out.println(m);
+    public static void main(String[]args){
+				Mazesolver f;
+				if(args.length < 1){
+						f = new Mazesolver("data1.dat");
+				}else{
+						f = new Mazesolver(args[0]);
+				}
+				f.clearTerminal();
+				f.solve();
+				
     }
+
 }
