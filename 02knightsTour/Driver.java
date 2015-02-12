b71429fc363b2deefce9ab3573057d55c69ced95
tonyli7 <xxtonyxxlixx@gmail.com>
2015-02-12 13:12:57 -0500

    commit

---
+++ 02knightsTour/Driver.java
@@ -0,0 +1,7 @@
+public class Driver{
+    public static void main(String[]args){
+	KnightsTour a=new KnightsTour(5);
+	a.solve();
+	System.out.println(a);
+    }
+}
--- 02knightsTour/KnightsTour.java
+++ 02knightsTour/KnightsTour.java
@@ -11,7 +11,7 @@ public class KnightsTour{
 
     //instance variable
     private int[][]board;
-    private int maxx,maxy;
+    private int size;
 
     //terminal specific character to move the cursor
     private String go(int x,int y){
@@ -28,32 +28,41 @@ public class KnightsTour{
 
     public String toString(){
 	String ans = "\n";
-	for (int i=0;i<board.length;i++){
-	    for (int j=0;j<board[0].length;j++){
-		ans+=board[i][j];
+	for (int i=0;i<size;i++){
+	    for (int j=0;j<size;j++){
+		ans+=board[i][j]+" ";
 	    }
 	    ans+="\n";
 	}
 	//build your knights tour here...
-	return hide + go(0,0) + ans + "\n" + show;
+	return hide + clear + go(0,0) + ans + "\n" + show;
     }
 
-    public KnightsTour(){
-	maxx=board[0].length;
-	maxy=board.length;
-    }
+   
 
     public KnightsTour(int size){
-				
+	this.size=size;
+	board=new int[size][size];
+	for (int i=0;i<size;i++){
+	    for (int j=0;j<size;j++){
+		board[i][j]=0;
+	    }
+	}
     }
 
     
-    public void solve(){
-	return solve(0,0);
+    public boolean solve(){
+	if (size<5){
+	    return false;
+	}
+	return solve(size/2,size/2);
     }
 
-    public void solve(int startx, int starty){
-
+    public boolean solve(int startx, int starty){
+	if (isFull()){
+	    return true;
+	}
+	return solve(startx,starty,0);
     }
 
 
@@ -65,7 +74,7 @@ public class KnightsTour{
 	if (isFull()){
 	    return true;
 	}
-	if (((x<maxx) && (x>-1)) || ((y<maxy) (y>-1)) && (board[y][x]==-1)){
+	if (((x<size) && (x>-1)) && ((y<size) && (y>-1)) && (board[y][x]==0)){
 	    board[y][x]=currentMoveNumber;
 	    if (solve(x+1,y-2,currentMoveNumber+1)||
 		solve(x-1,y-2,currentMoveNumber+1)||
@@ -74,17 +83,18 @@ public class KnightsTour{
 		solve(x+1,y+2,currentMoveNumber+1)||
 		solve(x-1,y+2,currentMoveNumber+1)||
 		solve(x-2,y+1,currentMoveNumber+1)||
-		solve(x-2,y-1,currentMoveNumber+1)||){
+		solve(x-2,y-1,currentMoveNumber+1)){
 		return true;
 	    }
+	    //  board[y][x]=currentMoveNumber-1;
 	}
 	return false;
     }
 
     public boolean isFull(){
-	for (int i=0;i<maxy;i++){
-	    for (int j=0;j<maxx;j++){
-		if (board[i][j]==-1){
+	for (int i=0;i<size;i++){
+	    for (int j=0;j<size;j++){
+		if (board[i][j]==0){
 		    return false;
 		}
 	    }
