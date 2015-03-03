f3cdac85dfd9938b884f7f640795e1306ddde8cf
tonyli7 <xxtonyxxlixx@gmail.com>
2015-03-03 01:50:26 -0500

    fixed mistakes, still doesnt work

--- 04USACO/ctravel.java
+++ 04USACO/ctravel.java
@@ -2,8 +2,11 @@ import java.util.*;
 public class ctravel{
     public static int rows,cols,sec;
     public static char grid[][];
+    public static boolean solution;
+    static int ways=0;
     public static void main(String[]args){
-	try{
+	
+	//	try{
 	    //	File file = new File("ctravel.in");
 	    //	Scanner sc = new Scanner(file);
 	    String input = "4 5 6\n...*.\n...*.\n.....\n.....\n1 3 1 5";
@@ -15,12 +18,14 @@ public class ctravel{
 	    sec=Integer.parseInt(line1.substring(line1.lastIndexOf(" ") + 1));
 	
 	    grid=new char[rows][cols];
-
+	    String g="";
 	    for (int y=0;y<rows;y++){
 		String l=sc.nextLine();
-		for (int x=0;y<cols;x++){
+		for (int x=0;x<cols;x++){
 		    grid[y][x]=l.charAt(x);
+		    g+=""+grid[y][x];
 		}
+		g+="\n";
 	    }
 
 	    String lastline = sc.nextLine();
@@ -29,30 +34,39 @@ public class ctravel{
 	    int c1 = Integer.parseInt(lastline.substring(lastline.indexOf(" ") + 1, lastline.indexOf(" ", lastline.indexOf(" ") + 1))) - 1;
 	    int r2 = Integer.parseInt(lastline.substring(lastline.indexOf(" ", lastline.indexOf(" ") + 1) + 1, lastline.lastIndexOf(" "))) - 1;
 	    int c2 = Integer.parseInt(lastline.substring(lastline.lastIndexOf(" ") + 1)) - 1;
-	  
-	    ctravel.solve(r1, c1, r2, c2, sec);
-	    
-	    System.out.println(ways);
-	}catch (Exception e){
-	    System.out.println("oops");
-	}
+	    solution=false;
+	    ctravel.solve(1, 3, 1, 5, 0);
+	    System.out.println(g);
+	    System.out.print(r1);
+	    System.out.println(c1);
+	    if (solution){
+		System.out.println(ways);
+	    }else{
+		System.out.println("gg");
+	    }
+	    //	}catch (Exception e){
+	    //	    System.out.println("oops");
+	    //	}
     }
-    static int ways=0;
-    public static boolean solve(int x1, int y1, int x2, int y2, int s){
+   
+    public static boolean solve(int y1, int x1, int y2, int x2, int s){
+	if ((x1<0) && (x1>=cols) &&
+	    (y1<0) && (y1>=rows) &&
+	    (grid[y1][x1]=='*') &&
+	    (s>sec)){
+	    return false;
+	}
 	if ((x1==x2) && (y1==y2) && (s==sec)){
 	    ways++;
+	    solution=true;
 	    return true;
 	}
-	if ((x1>=0) && (x1<cols) &&
-	    (y1>=0) && (y1<rows) &&
-	    (grid[y1][x1]!='*') &&
-	    (s<sec)){
-		
-	    solve(x1+1,y1,x2,y2,s+1);
-	    solve(x1-1,y1,x2,y2,s+1);
-	    solve(x1,y1+1,x2,y2,s+1);
-	    solve(x1,y1-1,x2,y2,s+1);
-	    return false;
+	if(
+	solve(y1+1,x1,y2,x2,s+1)||
+	solve(y1-1,x1,y2,x2,s+1)||
+	solve(y1,x1+1,y2,x2,s+1)||
+	solve(y1,x1-1,y2,x2,s+1)){
+	return true;
 	}
 	return false;
     }
