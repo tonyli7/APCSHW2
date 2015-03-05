945ba568cd89791b1e5840bff0888e03fe1eec6c
tonyli7 <xxtonyxxlixx@gmail.com>
2015-03-04 20:27:53 -0500

    Forgot to push actual mergeSort

--- 05mergeSort/Sorts.java
+++ 05mergeSort/Sorts.java
@@ -4,69 +4,77 @@ public class Sorts{
 	int[] ary=new int[4];
 	randomAry(20,ary);
 	System.out.println(toString(ary));
-	mergeSort(ary);
-	System.out.println(toString(ary));
+	int[] ar=new int[4];
+	randomAry(20,ar);
+	System.out.println(toString(ar));
+	mergeSort(ar);
+	System.out.println(toString(ar));
+	//	System.out.println(toString(merge(ary,ar)));
+	
     }
     
-    
-    public static void mergeSort(int[] ary){
-	ArrayList<Comparable>aL=new ArrayList<Comparable>();
-	for (int a: ary){
-	    aL.add(a);
-	}
-	mhelp(aL);
-	
-	for (int start=0;start<aL.size();start++){
-	    ary[start]=(int)aL.get(start);
-	}
-    }
+  
 
-    public static void mhelp(ArrayList<Comparable> aL){
-	ArrayList<Comparable>a=new ArrayList<Comparable>();
-	ArrayList<Comparable>b=new ArrayList<Comparable>();
-
-	if (aL.size()>1){
-	    //	aL.clear();
-	
-	    boolean even=true;
-	    for (int i=0;i<aL.size();i++){
-		if (even=!even){
-		    a.add(aL.get(i));
-		}else{
-		    b.add(aL.get(i));
-		}
-	    }
-	
+    public static void mergeSort(int[] a){
+	if (a.length<2){
+	    return;
 	}
-	mhelp(a);
-	mhelp(b);
-	
-       
-
-	int asize=a.size();
-	int bsize=b.size();
-	int ctr1=0;
-	int ctr2=0;
-	int ctr3=0;
-	while ((asize>0) && (bsize>0)){
-	    System.out.println(toString(aL));
-	    if (a.get(ctr1).compareTo(b.get(ctr2))<0){
-		aL.set(ctr3,a.get(ctr1));
-		ctr1++;
+	int[]p1=new int[a.length/2];
+	int[]p2=new int[a.length-p1.length];
+	int i1=0;
+	int i2=0;
+	for (int i=0;i<a.length;i++){
+	    if (i%2==0){
+		p1[i1]=a[i];
+		i1++;
 	    }else{
-		aL.set(ctr3,b.get(ctr2));
-		ctr2++;
+		p2[i2]=a[i];
+		i2++;
 	    }
-	    ctr3++;
-	    asize--;
-	    bsize--;
 	}
-	aL.addAll(a);
-	aL.addAll(b);
-	   
-    
+	if (!sorted(p1)){
+	    mergeSort(p1);
+	}
+	if (!sorted(p2)){
+	    mergeSort(p2);
+	}
+	int[]merged=merge(p1,p2);
+
+	for (int i=0;i<merged.length;i++){
+		a[i]=merged[i];
+	}
     }
 
+    public static int[] merge(int[] a, int[] b){
+	int[] merged=new int[a.length+b.length];
+	int asize=a.length;
+	int bsize=b.length;
+	int ai=0;
+	int bi=0;
+	int ctr=0;
+	while ((ai<asize) && (bi<bsize)){
+	    if (a[ai]<=b[bi]){
+		merged[ctr]=a[ai];
+		ai++;
+	    }else{
+		merged[ctr]=b[bi];
+		bi++;
+	    }
+	    ctr++;
+	}
+	if (asize>bsize){
+	    for (int i=ai;i<asize;i++){
+		merged[ctr]=a[i];
+		ctr++;
+	    }
+	}else{
+	    for (int i=bi;i<bsize;i++){
+		merged[ctr]=b[i];
+		ctr++;
+	    }
+	}
+	return merged;
+    }
     public static void randomAry(int range, int[] a){
 
 	Random rand=new Random();
@@ -77,16 +85,7 @@ public class Sorts{
 	
 	
     }
-
-    public static String toString(ArrayList<Comparable> c){
-	String list="[";
-	for (int i=0;i<c.size();i++){
-	    list+=""+c.get(i)+" ";
-	}
-	list+="]";
-	return list;
-    }
-
+  
     public static String toString(int[] a){
 	String list="[";
 	for (int i=0;i<a.length;i++){
@@ -94,5 +93,14 @@ public class Sorts{
 	}
 	list+="]";
 	return list;
+    }
+
+    public static boolean sorted(int[] a){
+	for (int i=0;i<a.length-1;i++){
+	    if (a[i]>a[i+1]){
+		return false;
+	    }
+	}
+	return true;
     }
 }
