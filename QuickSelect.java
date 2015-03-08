45da8a956ce8f434aaa2d8695b64c5f77434809b
tonyli7 <xxtonyxxlixx@gmail.com>
2015-03-08 13:51:01 -0400

    QuickSelect works

--- QuickSelect.java
+++ QuickSelect.java
@@ -11,16 +11,16 @@ public class QuickSelect{
 	a[5]=1;
 	a[6]=4;
 	a[7]=2;
-	System.out.println(toString(a));
-	partition(a,2,7);
+
+	System.out.println(QuickSelect(a,6));
 	System.out.println(toString(a));
     }
 
-    public static void partition(int[]ary,int si, int ei){
+    public static int partition(int[]ary,int si, int ei){
 	Random rand=new Random();
 	int pindex=rand.nextInt(ei-si+1)+si;
 	int pivot=ary[pindex];
-	System.out.println(pivot);
+
 	int gindex=ei;
 	int lindex=si;
 	ary[pindex]=ary[gindex];
@@ -37,10 +37,28 @@ public class QuickSelect{
 	temp=ary[lindex];
 	ary[lindex]=ary[gindex];
 	ary[gindex]=temp;
+	System.out.println(toString(ary)+" : "+pivot+" "+lindex);
+	return lindex;
     }
 
-    public static int QuickSelect(int kth,int[] a){
-	return 1;
+    public static int QuickSelect(int[] a, int n){
+	Random rand=new Random();
+	try{
+	    return QuickSelect(a,n,0,a.length-1);
+	}catch(IndexOutOfBoundsException e){
+	    throw new IndexOutOfBoundsException();
+	}
+    }
+
+    public static int QuickSelect(int[] a, int n, int li, int gi){
+	int temp=partition(a,li,gi);
+	if (temp==n-1){
+	    return a[n-1];
+	}else if(temp>n-1){
+	    return QuickSelect(a,n,li,temp-1);
+	}
+	return QuickSelect(a,n,temp+1,gi);
+	
     }
 
     public static String toString(int[]a){
