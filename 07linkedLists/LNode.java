9a144777b655b4dfa082a88b1c0366f9879eded5
tonyli7 <xxtonyxxlixx@gmail.com>
2015-03-11 23:59:38 -0400

    Fixed LNode, added add, toString, and size methods

--- 07linkedLists/LNode.java
+++ 07linkedLists/LNode.java
@@ -2,10 +2,17 @@ public class LNode{
     private int value;
     private LNode next;
 
-  
+    public LNode(int v){
+	this(v,null);
+    }
     
-    public void setNext(int n){
-	next.setValue(n);
+    public LNode(int v, LNode n){
+	value=v;
+	next=n;
+    }
+
+    public void setNext(LNode n){
+	next=n;
     }
 
     public void setValue(int v){
@@ -20,6 +27,8 @@ public class LNode{
 	return value;
     }
 
-
+    public String toString(){
+	return ""+value;
+    }
     
 }
--- 07linkedLists/MyLinkedList.java
+++ 07linkedLists/MyLinkedList.java
@@ -1,19 +1,42 @@
 public class MyLinkedList{
     private LNode head;
-    private LNode current;
+    private int size;
 
-    public MyLinkedList(int start){
-	head.setValue(start);
-	current.setValue(start);
+    public MyLinkedList(){
+	size=0;
     }
+
+    public MyLinkedList(int s){
+	size=s;
+    }
+
+   
     public String toString(){
-	String str=""+current.getValue+"";
-	return str;
+	String list="[";
+	LNode current=head;
+	while (current.getNext()!=null){
+	    list+=current.getValue()+" , ";
+	    current=current.getNext();
+	}
+	return list.substring(0,size-1)+"]";
     }
 
     public void add(int value){
-	while (current.getNext()!=null){
-	    head=current.getNext();
+	if (size==0){
+	    head=new LNode(value);
+	}else{
+	    LNode current=head;
+	    while (current.getNext()!=null){
+		current=current.getNext();
+	    }
+	    current.setNext(new LNode(value));
 	}
+	size++;
     }
+
+    public int size(){
+	return size;
+    }
+
+    
 }
