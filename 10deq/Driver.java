62f097faa0893598070961b1b2ef58ea85460c1d
tonyli7 <xxtonyxxlixx@gmail.com>
2015-03-25 13:04:44 -0400

    working AddFirst

---
+++ 10deq/Driver.java
@@ -0,0 +1,10 @@
+public class Driver{
+    public static void main(String[]args){
+	MyDEQ d=new MyDEQ(5);
+	d.addFirst(1);
+	d.addFirst(3);
+	System.out.println(d);
+
+
+    }
+}
--- 10deq/MyDEQ.java
+++ 10deq/MyDEQ.java
@@ -6,7 +6,30 @@ public class MyDEQ{
 
     public MyDEQ(int size){
 	deck=new int[size];
-	this.size=size;
+	this.size=0;
     }
     
+    public void addFirst(int value){	      
+	if (size==0){
+	    deck[0]=value;
+	    headpos=0;
+	    tailpos=0;
+	    size++;
+	}else{
+	    headpos++;
+	    deck[headpos]=value;
+	    size++;
+	}
+    }
+
+    public String toString(){
+	String deq="[ ";
+	for (int i=headpos;i<size;i++){
+	    deq+=deck[i]+" ";
+	}
+	for (int j=tailpos;j<headpos;j++){
+	    deq+=deck[j]+" ";
+	}
+	return deq+"]";
+    }
 }
