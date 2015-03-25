ec1d38639ca0f2b8a0af6aba6881bb43c45e4be0
tonyli7 <xxtonyxxlixx@gmail.com>
2015-03-25 12:48:00 -0400

    Constructor+instance variables

---
+++ 10deq/MyDEQ.java
@@ -0,0 +1,12 @@
+public class MyDEQ{
+    private int[] deck;
+    private int head,tail;
+    private int size;
+    private int headpos,tailpos;
+
+    public MyDEQ(int size){
+	deck=new int[size];
+	this.size=size;
+    }
+    
+}
