a681e27e4fc98891c483255dc06b9e691c2288c8
tonyli7 <xxtonyxxlixx@gmail.com>
2015-04-23 21:57:51 -0400

    Compiles

--- 12Trees/TreeNode.java
+++ 12Trees/TreeNode.java
@@ -1,14 +1,32 @@
+import java.util.*;
 public class TreeNode<E>{
     private TreeNode<E> prev;
+    private ArrayList<TreeNode<E>> next;
     private E data;
 
-    public TreeNode(){
-	this(null,null);
+    public TreeNode(E element){
+	this(element,null);
     }
 
     public TreeNode(E element, TreeNode<E> prev){
 	this.prev=prev;
 	data=element;
+	next=new ArrayList<TreeNode<E>>(2);
+    }
+
+    public boolean addData(E data){
+	if (next.size()==0){
+	    return false;
+	}
+	Random rand=new Random();
+	int rng=rand.nextInt(1);
+	next.set(rng, new TreeNode<E>(data, this));
+	return true;
+    }
+
+    public TreeNode<E> getRandChild(){
+	Random rand=new Random();
+	return next.get(rand.nextInt(1));
     }
 
     public void setData(E data){
