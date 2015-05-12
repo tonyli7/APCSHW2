e2c4788ecc7502e02af9bb2fedf6b88f561d9c06
tonyli7 <xxtonyxxlixx@gmail.com>
2015-05-12 13:14:26 -0400

    remove

--- 13Heaps/MyHeap.java
+++ 13Heaps/MyHeap.java
@@ -1,13 +1,15 @@
+import java.util.*;
 public class MyHeap{
     private int max,min;
-    private int[] heap;
+    private ArrayList<Integer> heap;
 
     public MyHeap(){
-	heap=int[10];
+	heap=new ArrayList<Integer>(10);
+	heap.add(0);
     }
 
     public MyHeap(boolean isMax){
-	super();
+	this();
 	if (isMax){
 	    max=20;
 	}else{
@@ -15,10 +17,27 @@ public class MyHeap{
 	}
     }
 
+    public int remove() {
+	if (heap.get(0)==0){
+	    throws new NullPointerException();
+	}else if(heap.get(0)==1){
+	    return heap.remove(1);
+	}else if(heap.get(0)==2){
+	    int temp=heap.remove(1);
+	    heap.set(1,heap.remove(2));
+	    return heap.get(1);
+	}else{
+	    
+	}
+    }
+
+    public void add(int){} 
+    public int peek() {}
+
     public String toString(){
 	String str="[";
-	for (int i=1;i<heap[0];i++){
-	    str+=heap[i]+" ";
+	for (int i=1;i<heap.get(0);i++){
+	    str+=heap.get(i)+" ";
 	}
 	return str+"]";
     }
