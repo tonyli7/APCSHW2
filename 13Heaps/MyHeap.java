27ea1f82db0085673c3d411cebc731b4643c9660
tonyli7 <xxtonyxxlixx@gmail.com>
2015-05-14 03:15:59 -0400

    gave up on toString()

--- 13Heaps/MyHeap.java
+++ 13Heaps/MyHeap.java
@@ -6,17 +6,6 @@ public class MyHeap{
 
     public MyHeap(){
 	heap=new ArrayList<Integer>(10);
-	
-	heap.add(7);
-	heap.add(20);
-	heap.add(10);
-	heap.add(15);
-	heap.add(9);
-	heap.add(8);
-	heap.add(14);
-	heap.add(13);
-	
-
 	isMax=true;
     }
 
@@ -103,11 +92,71 @@ public class MyHeap{
 	return root;
     }
 
+    public int linelength(){
+	int length=3;
+	int increment=4;
+	for (int i=2;i<size()/2;i++){
+	    length+=increment;
+	    increment+=4;
+	}
+	return length;
+    }
+
+    public int[] getLevel(int level){
+	double length=Math.pow(2,level-1);
+	int[] values=new int[(int)length];
+	for (double i=length, j=0;j<length;i++){
+	    values[(int)j]=heap.get((int)i);
+	    j++;
+	}
+	return values;
+    }
+    /*
     public String toString(){
 	String str="[";
 	for (int i=1;i<heap.get(0)+1;i++){
 	    str+=heap.get(i)+" ";
 	}
 	return str+"]"+"\n\n"+heap.get(0);
+    }
+    */
+    public String toString(){
+	//	String pile[][]=new String[size()/2][linelength()];
+	String tree="";
+	int spacing=linelength()/2;
+	int middlespace=spacing*2;
+	int next=0;
+	System.out.println(size());
+	for (int i=0;i<size()/2;i++){
+	    for (int j=0;j<linelength();j++){
+		if (i==0){
+		    if (spacing>0){
+			tree+=" ";
+			spacing--;
+		    }
+		    else if (spacing==0){
+			tree+=getLevel(i)[next];
+			next++;
+			spacing=linelength()/2;
+		    }
+		}else{
+		    if (spacing>0){
+			tree+=" ";
+			spacing--;
+		    }else if (spacing==0){
+			tree+=getLevel(i)[next];
+			next++;
+		    }else if (spacing==0 && middlespace!=0){
+			tree+=" ";
+			middlespace--;
+		    }
+		}
+	    }
+	    spacing--;
+	    middlespace=middlespace/2;
+	    next=0;
+	    tree+="\n";
+	}
+	return tree;
     }
 }
